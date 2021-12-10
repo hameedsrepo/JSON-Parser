bea479f1da94b0fb5d2d78d8ff3f1554d042bbaf
hameed <afg_ham@yahoo.com>
2021-12-09 21:47:15 -0700

    changed to use relative paths

--- BlackBoxTesting/Program.cs
+++ BlackBoxTesting/Program.cs
@@ -9,12 +9,21 @@ namespace BlackBoxTesting
         static void Main(string[] args)
         {
             //0. Setup
-            string pathToApp = @"C:\Users\ps3se\source\repos\JSON Parser\JSON Parser\bin\Debug\net5.0\JSON Parser.exe"; //change so we can use relative paths
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            string pathToFile = System.IO.Path.Combine(baseDir, @"..\..\..\Test Files\test.json");
+            pathToFile = System.IO.Path.GetFullPath(pathToFile);
+            pathToFile = "\"" + pathToFile + "\""; //required for parameter of Main with spaces, otherwise will split up
+
+            string pathToApp = System.IO.Path.Combine(baseDir, @"..\..\..\..\JSON Parser\bin\Debug\net5.0\JSON Parser.exe");
+            pathToApp = System.IO.Path.GetFullPath(pathToApp);
+
+            Console.WriteLine($"Path to File: {pathToFile} \nPath To App: {pathToApp}");//this works!
             ProcessStartInfo startInfo = new ProcessStartInfo(pathToApp);
             //1. run JSON Parser with parameters we want (our text file is fine)
             //https://stackoverflow.com/questions/186822/capturing-console-output-from-a-net-application-c
-            
-            startInfo.Arguments = @"Z:\test.json"; //change so we can use relative paths
+
+            startInfo.Arguments = pathToFile;
             Process.Start(startInfo);
 
             //2. output the console into something readable but this applicaiton
